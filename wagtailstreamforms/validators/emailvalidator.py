fd6002e493abbc07cbd8fe2136098abdc3825023
hochshi <hochshi@users.noreply.github.com>
2019-08-07 15:31:30 +0300

    Fixed email domain validation.

--- wagtailstreamforms/validators/emailvalidator.py
+++ wagtailstreamforms/validators/emailvalidator.py
@@ -1,4 +1,6 @@
 import re
+from typing import Optional, Collection
+
 from django.core import validators
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
@@ -7,10 +9,15 @@ from django.utils.translation import gettext_lazy as _
 class EmailDomainWhiteListValidator(validators.EmailValidator):
     message = _('Email domain is not approved.')
 
+    def __init__(self, message: Optional[str] = None, code: Optional[str] = None,
+                 whitelist: Optional[Collection[str]] = None) -> None:
+        super().__init__(message, code, whitelist)
+        self.domain_pattern = '|'.join(self.domain_whitelist).replace('.', '\.')
+
     def __call__(self, value):
         super().__call__(value)
         user_part, domain_part = value.rsplit('@', 1)
-        if not re.search(domain_part, ';'.join(self.domain_whitelist)):
+        if not re.search(self.domain_pattern, domain_part):
             raise ValidationError(self.message, code=self.code)
         return
 
